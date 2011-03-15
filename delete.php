97f256f9aff309c116537844446c4c443769edf3
Caleb Fidecaro <calebfidecaro@gmail.com>
2011-03-15 20:42:00 +1300

    Updated

--- delete.php
+++ delete.php
@@ -1,3 +1,13 @@
 <?php
-	
+include 'twitter.php';
+$twitter = new Twitter('jGrDrZUnUQAkGeGdsZo7Og', 'n44LC4FcDsPUyWkkjcR00Dtz1ZncRHAlQSPOE0wsIs');
+$twitter->setOAuthToken($_SESSION['oauth_token']);
+$twitter->setOAuthTokenSecret($_SESSION['oauth_token_secret']);
+
+foreach($_REQUEST['to_delete'] as $delete_id)
+{
+	$twitter->directMessagesDestroy($delete_id);
+}
+
+echo 'success';
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,109 @@
 <?php
+session_start();
 include 'twitter.php';
-
-// create instance
 $twitter = new Twitter('jGrDrZUnUQAkGeGdsZo7Og', 'n44LC4FcDsPUyWkkjcR00Dtz1ZncRHAlQSPOE0wsIs');
 
-// get a request token
-$twitter->oAuthRequestToken('http://caleb.wasabi.cc/dm_deleter');
+if(isset($_SESSION['oauth_token']) && isset($_SESSION['oauth_token_secret']))
+{	
+	$twitter->setOAuthToken($_SESSION['oauth_token']);
+	$twitter->setOAuthTokenSecret($_SESSION['oauth_token_secret']);
+}
 
-// authorize
-if(!isset($_GET['oauth_token'])) $twitter->oAuthAuthorize();
+else
+{
+	$twitter->oAuthRequestToken('http://caleb.wasabi.cc/dm_deleter');
+	if(!isset($_GET['oauth_token'])) $twitter->oAuthAuthorize();
+	$response = $twitter->oAuthAccessToken($_GET['oauth_token'], $_GET['oauth_verifier']);
+	
+	$_SESSION['oauth_token'] = $response['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $response['oauth_token_secret'];
+	$twitter->setOAuthToken($_SESSION['oauth_token']);
+	$twitter->setOAuthTokenSecret($_SESSION['oauth_token_secret']);
+}
 
-// get tokens
-$response = $twitter->oAuthAccessToken($_GET['oauth_token'], $_GET['oauth_verifier']);
+$dms = array();
 
-$twitter->setOAuthToken($response['oauth_token']);
-$twitter->setOAuthTokenSecret($response['oauth_token_secret']);
+for($i = 1; $i <= 25; $i++)
+{
+	$dms = array_merge($dms, $twitter->directMessages(null, null, 200, $i));
+}
 
-$dms = $twitter->directMessages(null, null, 200, 3);
-echo '<pre>';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DM Deleter</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+	<script>
+	$(function(){
+		$('.select_all').change(function(){
+			if($(this).is(':checked'))
+			{
+				$('.checker').attr('checked', true).trigger('change');
+			}
+			
+			else
+			{
+				$('.checker').attr('checked', false).trigger('change');
+			}
+		});
+		
+		$('#the_form').submit(function(){
+			return false;
+		});
+		
+		$('.go').click(function(){
+			var to_delete = $('#the_form').serializeArray();
+			$('.checker, .select_all').attr('disabled', 'disabled');
+			
+			$.post('delete.php', to_delete, function(){
+				$('.checker:checked').parent().parent().slideUp().remove();
+				$('.checker, .select_all').removeAttr('disabled');
+			}, 'json');
+		});
+	});
+	</script>
+</head>
+<body>
+	<form method="post" action="#" id="the_form">
+	<table border="0" cellspacing="0" cellpadding="5">
+		<thead style="background-color: #ccc;">
+			<tr>
+				<th><input type="checkbox" class="select_all" value="true" /></th>
+				<th>Received</th>
+				<th>From</th>
+				<th>Message (<?=count($dms)?> total)</th>
+				<th><input type="button" class="go" value="Do it!" /></th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php foreach($dms as $dm): ?>
+			<tr>
+				<td><input type="checkbox" name="to_delete[]" class="checker" value="<?=$dm['id']?>" /></td>
+				<td><?=date('j/n/Y', strtotime($dm['created_at']))?></td>
+				<td><?=$dm['sender']['name']?> (<?=$dm['sender']['screen_name']?>)</td>
+				<td colspan="2"><?=$dm['text']?></td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+		<tfoot style="background-color: #ccc;">
+			<tr>
+				<th><input type="checkbox" class="select_all" value="true" /></th>
+				<th>Received</th>
+				<th>From</th>
+				<th>Message</th>
+				<th><input type="button" class="go" value="Do it!" /></th>
+			</tr>
+		</tfoot>
+	</table>
+	</form>
+	
+	<ul id="checked"></ul>
+</body>
+</html>
+<?php
+
+/*echo '<pre>';
 var_dump($dms);
-echo '</pre>';
+echo '</pre>';*/
 ?>
